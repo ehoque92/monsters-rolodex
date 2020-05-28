af7a804d3e457ce3c24b53c85e93ab68632f87ce
Ennam Hoque <ennamhoque@ennams-mbp.home>
2020-05-28 01:31:53 +0100

    Updates

---
+++ precache-manifest.63d9ec11197d1a908e77065b80578299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3484eb10d4d59661c803a0e7abcb87d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2744a242300982f64556",
+    "url": "/monster-rolodex/static/css/main.7772c043.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2744a242300982f64556",
+    "url": "/monster-rolodex/static/js/main.9a5f8e3b.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
